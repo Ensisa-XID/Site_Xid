7d3f83e0370740522e769b3cb9b678211c50afd5
Gael Alberola <zoski@free.fr>
2013-11-29 18:00:50 +0100

    Icones, présentation mise à jour

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include "includes/header.php" ?>
+<?php include "includes/nav.php" ?>
+<?php include "includes/acceuil.php" ?>
+<?php include "includes/event.php" ?>
+<?php include "includes/contact.php" ?>
