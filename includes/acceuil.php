076cda9ddfc6ab26278e9d3959a7bad61455c726
Gael Alberola <zoski@free.fr>
2013-12-01 18:09:17 +0100

    nettoyage + ajout animation logo du Xid

--- includes/acceuil.php
+++ includes/acceuil.php
@@ -1,36 +1,24 @@
-<div class="container">
-	<div class="starter-template">
-		<h1>Le club d'informatique de l'ENSISA</h1>
-		<p class="lead">Destiné aux élèves qui souhaitent s'amèliorer dans tous les domaines de l'informatique... ou jouer ;-)</p>
-	</div>
+	<div class="container">
+		<div class="starter-template">
+			<h1>Le club d'informatique de l'ENSISA</h1>
+			<p class="lead">Destiné aux élèves qui souhaitent s'amèliorer dans tous les domaines de l'informatique... ou jouer ;-)</p>
+		</div>
 
-<section id="accueil"> 
 	<div class="row">
 		<div class="col-md-4"><!-- Panneau de gauche -->	
-			<div id="logo">
+			<div id="logo" class="expandUp">
 				<img src="img/logoxid.png" alt="Logo"/>
 			</div>
 		</div>
 				
 		<div class="col-md-8"><!-- Panneau de droite -->
-			<p> Le Xid était un club totalement inactif il y a encore quelques années, ses locaux servaient surtout de débarats pour la matériel informatique. 
+			<p>Le Xid était un club totalement inactif il y a encore quelques années, ses locaux servaient surtout de débarats pour la matériel informatique. 
 				Depuis 2011, le club a repris du service et connait chaque année une évolution impressionnante.</p>
 			<p>Et pourquoi tant de personnes viennent ?!!! </p>
 			<ul>
-				<li>  Un point de rassemblement où toutes les promotions peuvent échanger, rigoler. Le Xid c'est notre CyberCafé à nous ;) </li>					
-				<li> Une source d'information inépuisable, les plus expérimentés aident les curieux. Pour apprendre, seule la motivation compte ! </li>
-				<li> Une dizaine de gamers qui viennent au quotidient, nous pouvons atteindre la trentaine lors des heures de pointe.</li>
+				<li>Un point de rassemblement où toutes les promotions peuvent échanger, rigoler. Le Xid c'est notre CyberCafé à nous ;) </li>					
+				<li>Une source d'information inépuisable, les plus expérimentés aident les curieux. Pour apprendre, seule la motivation compte ! </li>
+				<li>Une dizaine de gamers qui viennent au quotidient, nous pouvons atteindre la trentaine lors des heures de pointe.</li>
 			</ul>
-				
-				<!-- MAIS A QUOI CE SERT ? xD -> ca sert à rien c'était juste pour m'entrainer à faire un bouton :D
-				<a href="http://batman-ipsum.com/">
-					<button class="btn btn-default btn-lg" href="http://batman-ipsum.com/">Batman Ipsum 
-					<span class="glyphicon glyphicon-link"></span>
-					</button>
-				</a>
-				 -->
-				
-
 		</div> 
 	</div>
-</section><!-- /.section accueil -->
--- includes/nav.php
+++ includes/nav.php
@@ -11,7 +11,7 @@
         </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav"> <!-- Pb au niveau des ancres ! La raison c'est le navigateur qui prend la place du haut et qui cache le début du texte-->
-            <li><a href="#accueil"><i class="fa fa-home fa-lg"></i> Accueil</a></li>
+            <li><a href="index.php"><i class="fa fa-home fa-lg"></i> Accueil</a></li>
             <li><a href="#event"><i class="fa fa-calendar fa-lg"></i> Événements</a></li>
             <li><a href="#contact"><i class="fa fa-comment-o fa-lg"></i> Contact</a></li>
           </ul>
