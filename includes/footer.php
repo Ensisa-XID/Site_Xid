fc13471e3632ea147a359e3811c762cfda1557f5
Gael Alberola <zoski@free.fr>
2013-12-04 14:30:32 +0100

    PAN le <span> qui était perdu && maj de l'indentation

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
-	</div><!-- /.container -->
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-  </body>
+		</div><!-- /.container -->
+	    <!-- Bootstrap core JavaScript
+	    ================================================== -->
+	    <!-- Placed at the end of the document so the pages load faster -->
+	    <script src="js/jquery.js"></script>
+	    <script src="js/bootstrap.min.js"></script>
+	</body>
 </html>
